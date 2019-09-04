d53d1cef286c408ddca8111fbcdfbf9211df7723
Cai heng <ianyaheng822@gmail.com>
2019-09-04 12:29:31 +1000

    finish the laundry(1 item) display method

--- dist/drop_off_service/test.rb
+++ dist/drop_off_service/test.rb
@@ -2,8 +2,8 @@
 require "tty-prompt"
 require 'colorize'
 require 'terminal-table'
-
-
+require 'date'
+require 'terminal-table'
 
 
 
@@ -21,42 +21,90 @@ customers=[
 ]
 
 
-def add_customer(customers)
-    puts "Sorry, you havn't been to our shop before".colorize(:green)
-    puts "we need record your information first".colorize(:green) 
-    puts "Can I have your name please".colorize(:green) 
-    puts "> "
-    name=gets.strip()
-    puts "Can I have your phone number please".colorize(:green) 
-    puts "> "
-    phone_number=gets.strip()
-    customer_code=rand(0..1000)
+# def add_customer(customers)
+#     puts "Sorry, you havn't been to our shop before".colorize(:green)
+#     puts "we need record your information first".colorize(:green) 
+#     puts "Can I have your name please".colorize(:green) 
+#     puts "> "
+#     name=gets.strip()
+#     puts "Can I have your phone number please".colorize(:green) 
+#     puts "> "
+#     phone_number=gets.strip()
+#     customer_code=rand(0..1000)
 
-    customer={
-        name:name,
-        phone_number:phone_number.to_s,
-        customer_code:customer_code.to_s
-    }
+#     customer={
+#         name:name,
+#         phone_number:phone_number.to_s,
+#         customer_code:customer_code.to_s
+#     }
 
-     puts "you are already in our system, THANKS".colorize(:green)
-     puts customers
+#      puts "you are already in our system, THANKS".colorize(:green)
+#      puts customers
     
+# end
+# item_choose=["shirt"]
+#     item_choose=["shirt","t-shirt"]
+   
+
+
+
+
+
+# def laundry_service()
+#     item_choose=[]
+#     loop do
+#         prompt = TTY::Prompt.new
+#         item=prompt.select("Choose your item need to laundry?", %w(SHIRTS/SHORT SHIRTS/LONG PANTS/NO/STARCH PANTS/MED 
+#             SHORTS/NO/STARCH SHORTS/MED/STARCH LAB/COAT/LIGHT/STARCH JACKET/LIGHT SKIRTS DRESSES DRESSES/HEAVY/STARCH CAMMIES/HEAVY/TARCH DRAPES
+#             PILLOW/CASES)) 
+       
+#         answer = prompt.select('Any other garment need to laundry?', %w(Yes No))
+#         if answer=="No"
+#            p item_choose.push(item)
+#             return laundry_display(item_choose)
+#         else answer="Yes" 
+#             p item_choose.push(item)
+#         end  
+#     end
+      
+# end
+# laundry_service()
+
+def laundry_items(item_choose)
+
+    total_price=0
+    if item_choose.length==1
+    File.open('laundry_price.csv',"r").each_with_index do |item,index|
+        if item.include?(item_choose[0])
+            puts "true"
+            price=item.split(',')[3]
+            total_price=price
+            drop_off_date=Date.today()
+            table = Terminal::Table.new do |t|
+                t.add_row ["Item","Price","Tota;","Drop_Off Date","Delieved Date"]
+                t.add_row [item_choose[0],price,total_price,drop_off_date]
+
+                t.style = {:all_separators => true}
+            end
+          puts table
+         
+        end  
+        end 
+    else 
+        # File.open('laundry_price.csv',"r").each do |item|
+        #     item_choose.each_with_index do |item,index|
+        #         item.include?(item_choose[index])
+        #         price=item.split(',')[3]
+        #         total_price +=price
+
+        #     end
+        # end
+      
+    end 
+
+
 end
-
-def laundry_service()
-    prompt = TTY::Prompt.new
-    service_choice=prompt.select("Choose your item nned to laundry?", %w(SHIRTS-SHORT SLEEVE SHIRTS-LONG SLEEVE 
-        PANTS-MED SHORTS-NO SHORTS-MED COAT- SHORT-NO COAT-LONG-HEAVY JACKET SKIRTS DRESSES-HEAVY CAMMIES-NO CAMMIES-HEAVY
-        TABLE-CLOTHS PILLOW CASES))   
-    
-
-
-
-    
-    
-    
-end
-laundry_service()
+laundry_items(["SHIRTS/SHORT"])
 
 
 # def press_only()
