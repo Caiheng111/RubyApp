35b4a91fb954e5122e54aa2c2537e958441aa5a2
Cai heng <ianyaheng822@gmail.com>
2019-09-04 10:08:41 +1000

    service choice setup

--- dist/drop_off_service/test.rb
+++ dist/drop_off_service/test.rb
@@ -8,6 +8,7 @@ require 'terminal-table'
 
 
 
+
 customers=[
     {name:"Heng",phone_number:"0410780822",customer_code:"123"},
     {name:"zhao",phone_number:"0417827648",customer_code:"234"},
@@ -43,41 +44,51 @@ def add_customer(customers)
 end
 
 def laundry_service()
-    puts ""
+    prompt = TTY::Prompt.new
+    service_choice=prompt.select("Choose your item nned to laundry?", %w(SHIRTS-SHORT SLEEVE SHIRTS-LONG SLEEVE 
+        PANTS-MED SHORTS-NO SHORTS-MED COAT- SHORT-NO COAT-LONG-HEAVY JACKET SKIRTS DRESSES-HEAVY CAMMIES-NO CAMMIES-HEAVY
+        TABLE-CLOTHS PILLOW CASES))   
+    
+
+
+
+    
+    
     
 end
+laundry_service()
 
 
-def press_only()
-    p "dry clean"
-end
+# def press_only()
+#     p "dry clean"
+# end
 
 
-def dry_clean()
-    p "dry-clean"
-end
+# def dry_clean()
+#     p "dry-clean"
+# end
 
 
-def drop_off(customers)
-    puts "What is your Phone number or customer_code?"
-    puts "> "
-    number_input=gets.strip()
-    customers.each do |customer|
-        if number_input==customer[:phone_number] ||number_input==customer[:customer_code]
-                # p "ok " input the customer number code and name
-                # input the garments
-                puts "THANKS, you are already our customer"
-                prompt = TTY::Prompt.new
-                choice=prompt.select("What kind of service do you need?", %w(Laundry press_only dry_clean))
-                if choice=="Laundry"
-                    laundry_service()
-                elsif choice=="press_only"
-                    press_only()
-                else choice=="dry_clean"
-                    dry_clean()
-                end
-            end
-        end
-       add_customer(customers)     
-end
-drop_off(customers)
+# def drop_off(customers)
+#     puts "What is your Phone number or customer_code?"
+#     puts "> "
+#     number_input=gets.strip()
+#     customers.each do |customer|
+#         if number_input==customer[:phone_number] ||number_input==customer[:customer_code]
+#                 # p "ok " input the customer number code and name
+#                 # input the garments
+#                 puts "THANKS, you are already our customer"
+#                 prompt = TTY::Prompt.new
+#                 choice=prompt.select("What kind of service do you need?", %w(Laundry press_only dry_clean))
+#                 if choice=="Laundry"
+#                     laundry_service()
+#                 elsif choice=="press_only"
+#                     press_only()
+#                 else choice=="dry_clean"
+#                     dry_clean()
+#                 end
+#             end
+#         end
+#        add_customer(customers)     
+# end
+# drop_off(customers)
