8db83c712399741f9e64abd1cfa99de6f09ce63e
Cai heng <ianyaheng822@gmail.com>
2019-09-04 20:56:01 +1000

    the laundry service display method

--- dist/drop_off_service/laundry_display.rb
+++ dist/drop_off_service/laundry_display.rb
@@ -3,6 +3,7 @@ require 'colorize'
 require 'terminal-table'
 require 'date'
 
+
 def laundry_display(item_choose)
     total_price=0
     if item_choose.length==1
@@ -16,25 +17,25 @@ def laundry_display(item_choose)
 
                 prompt = TTY::Prompt.new
                 pickup_date=prompt.select("When would like to pich up your garments?", %w(tomorrow one_day_later two_days_days))
-                puts "Tanks, this is your ticket,please keep the ticket to pich up your garments "
+                # puts "Tanks, this is your ticket,please keep the ticket to pich up your garments "
                 if pickup_date=="tomorrow"
                     table = Terminal::Table.new do |t|
-                        t.add_row ["Ticket_number","Item","Price","Total","Drop_Off Date","Delieved Date"]
-                        t.add_row [ticket_number,item_choose[0],price,total_price,drop_off_date,drop_off_date+1]
+                        t.add_row ["Ticket_number","Item","Price","Drop_Off Date","Delieved Date","Total($)"]
+                        t.add_row [ticket_number,item_choose[0],price,drop_off_date,drop_off_date+1,total_price]
                         t.style = {:all_separators => true}
                     end
                     puts table
                 elsif pickup_date=="one_day_later"
                     table = Terminal::Table.new do |t|
-                        t.add_row ["Ticket_number","Item","Price","Total","Drop_Off Date","Delieved Date"]
-                        t.add_row [ticket_number,item_choose[0],price,total_price,drop_off_date,drop_off_date+2]
+                        t.add_row ["Ticket_number","Item","Price","Drop_Off Date","Delieved Date","Total($)",]
+                        t.add_row [ticket_number,item_choose[0],price,drop_off_date,drop_off_date+2,total_price]
                         t.style = {:all_separators => true}
                     end
                     puts table
                 else pickup_date=="two_days_days"
                     table = Terminal::Table.new do |t|
-                        t.add_row ["Ticket_number","Item","Price","Total","Drop_Off Date","Delieved Date"]
-                        t.add_row [ticket_number,item_choose[0],price,total_price,drop_off_date,drop_off_date+3]
+                        t.add_row ["Ticket_number","Item","Price","Drop_Off Date","Delieved Date","Total($)"]
+                        t.add_row [ticket_number,item_choose[0],price,drop_off_date,drop_off_date+3,total_price]
                         t.style = {:all_separators => true}
                     end
                     puts table
@@ -44,28 +45,27 @@ def laundry_display(item_choose)
             end 
     
     else item_choose.length > 0
+        rows=[]
+        total_price=0
         File.open('laundry_price.csv',"r").each do |item1|
-            item_choose.each_with_index do |item2,index|
-                
+            item_choose.each do |item2|
                 if item1.include?(item2)
-                price=item1.split(',')[3]
+                price=item1.split(',')[3].to_i
                 drop_off_date=Date.today()
-                 ticket_number=rand(0..10000)
-                item_array=[ticket_number,item2,price,drop_off_date,drop_off_date+1]
-                item_array_total=item_array_total.push(item_array)
-
-             
-                # prompt = TTY::Prompt.new
-                # pickup_date=prompt.select("When would like to pich up your garments?", %w(tomorrow one_day_later two_days_days))
-                # puts "Tanks, this is your ticket,please keep the ticket to pich up your garments "
-                table = Terminal::Table.new :headings => ["Ticket_number","Item","Price","Total","Drop_Off Date","Delieved Date"], :rows => item_array_total
-
-                 puts table
-                end
+                item_array=[item2,price,drop_off_date,drop_off_date+2]
+                rows=rows.push(item_array)
+                total_price+=price 
+                end  
                 
-
             end
         end 
+        
+        ticket_number=rand(0..10000)
+        puts "The ticket number is #{ticket_number}".colorize(:green)
+        table = Terminal::Table.new :headings => ["Item","Price","Drop_Off Date","Delieved Date"], :rows => rows
+        puts table
+        puts "The total price is #{total_price}".colorize(:green)
+       
     end
             
 end
