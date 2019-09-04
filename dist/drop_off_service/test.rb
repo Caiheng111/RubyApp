2c4e6ef5b8ac7b5ff8f351d634c40732dcf9b4da
Cai heng <ianyaheng822@gmail.com>
2019-09-04 14:27:53 +1000

    the laundry (more than two items) method

--- dist/drop_off_service/test.rb
+++ dist/drop_off_service/test.rb
@@ -3,10 +3,6 @@ require "tty-prompt"
 require 'colorize'
 require 'terminal-table'
 require 'date'
-require 'terminal-table'
-
-
-
 
 
 customers=[
@@ -42,11 +38,73 @@ customers=[
 #      puts customers
     
 # end
-# item_choose=["shirt"]
-#     item_choose=["shirt","t-shirt"]
-   
 
+def laundry_items(item_choose)
+    total_price=0
+    if item_choose.length==1
+        File.open('laundry_price.csv',"r").each_with_index do |item,index|
+            if item.include?(item_choose[0])
+                puts "true"
+                price=item.split(',')[3]
+                total_price=price
+                drop_off_date=Date.today()
+                ticket_number=rand(0..10000)
 
+                prompt = TTY::Prompt.new
+                pickup_date=prompt.select("When would like to pich up your garments?", %w(tomorrow one_day_later two_days_days))
+                puts "Tanks, this is your ticket,please keep the ticket to pich up your garments "
+                if pickup_date=="tomorrow"
+                    table = Terminal::Table.new do |t|
+                        t.add_row ["Ticket_number","Item","Price","Total","Drop_Off Date","Delieved Date"]
+                        t.add_row [ticket_number,item_choose[0],price,total_price,drop_off_date,drop_off_date+1]
+                        t.style = {:all_separators => true}
+                    end
+                    puts table
+                elsif pickup_date=="one_day_later"
+                    table = Terminal::Table.new do |t|
+                        t.add_row ["Ticket_number","Item","Price","Total","Drop_Off Date","Delieved Date"]
+                        t.add_row [ticket_number,item_choose[0],price,total_price,drop_off_date,drop_off_date+2]
+                        t.style = {:all_separators => true}
+                    end
+                    puts table
+                else pickup_date=="two_days_days"
+                    table = Terminal::Table.new do |t|
+                        t.add_row ["Ticket_number","Item","Price","Total","Drop_Off Date","Delieved Date"]
+                        t.add_row [ticket_number,item_choose[0],price,total_price,drop_off_date,drop_off_date+3]
+                        t.style = {:all_separators => true}
+                    end
+                    puts table
+                end
+
+            end  
+            end 
+    
+    else item_choose.length > 0
+        File.open('laundry_price.csv',"r").each do |item|
+            item_choose.each_with_index do |item_choose,index|
+                if item==item_choose
+                # item.include?(item_choose[index])
+                price=item.split(',')[3]
+                total_price =price
+                ticket_number=rand(0..10000)
+                item_array=[ticket_number,item_choose[0],price,total_price,drop_off_date,drop_off_date]
+                item_array_total=item_array_total.push(item_array)
+                
+                # prompt = TTY::Prompt.new
+                # pickup_date=prompt.select("When would like to pich up your garments?", %w(tomorrow one_day_later two_days_days))
+                # puts "Tanks, this is your ticket,please keep the ticket to pich up your garments "
+                table = Terminal::Table.new :headings => ["Ticket_number","Item","Price","Total","Drop_Off Date","Delieved Date"], :rows => item_array_total
+
+                 puts table
+                end
+                
+
+            end
+        end 
+    end 
+            
+end
+laundry_items(["SHIRTS/SHORT","SKIRTS"]) 
 
 
 
@@ -70,49 +128,15 @@ customers=[
 # end
 # laundry_service()
 
-def laundry_items(item_choose)
-
-    total_price=0
-    if item_choose.length==1
-    File.open('laundry_price.csv',"r").each_with_index do |item,index|
-        if item.include?(item_choose[0])
-            puts "true"
-            price=item.split(',')[3]
-            total_price=price
-            drop_off_date=Date.today()
-            table = Terminal::Table.new do |t|
-                t.add_row ["Item","Price","Tota;","Drop_Off Date","Delieved Date"]
-                t.add_row [item_choose[0],price,total_price,drop_off_date]
-
-                t.style = {:all_separators => true}
-            end
-          puts table
-         
-        end  
-        end 
-    else 
-        # File.open('laundry_price.csv',"r").each do |item|
-        #     item_choose.each_with_index do |item,index|
-        #         item.include?(item_choose[index])
-        #         price=item.split(',')[3]
-        #         total_price +=price
-
-        #     end
-        # end
-      
-    end 
 
 
-end
-laundry_items(["SHIRTS/SHORT"])
+
+# # def press_only()
+# #     p "dry clean"
+# # end
 
 
-# def press_only()
-#     p "dry clean"
-# end
-
-
-# def dry_clean()
+# # def dry_clean()
 #     p "dry-clean"
 # end
 
