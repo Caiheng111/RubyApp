4ff4ad3d4ae51dddb893ce849aef37c579d51a57
Cai heng <ianyaheng822@gmail.com>
2019-09-05 16:15:12 +1000

    set the check_password function

--- test/app_test.rb
+++ test/app_test.rb
@@ -5,6 +5,7 @@ require 'colorize'
 require 'terminal-table'
 require 'csv'
 require_relative '../dist/drop_off_service/drop_off_service.rb'
+require 'pry'
 
 
 
@@ -43,24 +44,7 @@ puts font.write("WELCOME")
 puts font.write("                 H   Y ")
 puts font.write("DRY   CLEANING")
 
-users=[
-    user1={
-        name:"Heng",
-        password:"admin123"
-    },
-    user2={
-        name:"Hannah",
-        password:"admin719"
-    },
-    user3={
-        name:"Upa",
-        password:"admin789"
-    },
-    user4={
-        name:"Hevvon",
-        password:"admin467"
-    }
-]
+
 
 
 
@@ -77,59 +61,54 @@ def reset_password()
             password_two=gets.strip()
             if password_one==password_two
                 users.each do |user|
-                    if user[:nmae]==name
+                    if user[:name]==name
                     users[:password]=password_one
-                    puts "You already reseted your password," 
+                    puts "You already reseted your password.".colorize(:green)
                     break
-                    elsif
-                    password_one != password_two  
+
+                    end
                end  
             end
         time+=1
         end
-    end
 end
 
-
-
-def log_in(users)
-
-    prompt = TTY::Prompt.new
-    name=prompt.select("Please choose the staff name?", %w(Heng  Hannah Upa Hevvon))
+def check_password(users,name)
     puts "please input your password"
     password=gets.strip()
+    found_user = users.find do |user|
+        user[:name] == name
+    end
 
-        users.each do |user|
-            if name=user[:name] && password==user[:password]
-           
-                puts "THANKS,#{name},You already logined." 
-                break
-            else
-                reset_password()
-               
-            end
-
-        end
+    if found_user[:password] == password
+        return found_user
+    else
+        puts "wrong password"
+       
+    end
 
 end
+
+
+
 
 def service_view()
 
     prompt = TTY::Prompt.new
-    choice=prompt.select("What cleaning service do you need?", %w(Laundry press_only dry_clean))
+    choice=prompt.select("What cleaning service do you need?", %w(Laundry Press_only Dry_clean))
     if choice=="Laundry"
         csv=CSV.read("laundry_price.csv")
         heading=csv.first
         rows=csv[1..-1]
         table1 = Terminal::Table.new :headings => heading, :rows => rows
             puts table1
-    elsif choice=="press_only"
+    elsif choice=="Press_only"
         csv=CSV.read("press_only.csv")
         heading=csv.first
         rows=csv[1..-1]
         table2 = Terminal::Table.new :headings => heading, :rows => rows
             puts table2
-    else choice=="dry_clean"
+    else choice=="Dry_clean"
         csv=CSV.read("dry_cleaning.csv")
         heading=csv.first
         rows=csv[1..-1]
@@ -139,29 +118,55 @@ def service_view()
 end
 
 
-
-def option(users,customers)
-    
-    prompt = TTY::Prompt.new
-    choose=prompt.select("Are you a staff or a customer?", %w(Staff Customer Exit))
-    if choose=="Staff"
-        log_in(users)
-        service_view()
-        
-
-    elsif choose=="Customer"
+def service(customers)
     loop do
         prompt = TTY::Prompt.new
-        service_choice=prompt.select("Please choose your service?", %w(Service_view drop_off_garments))   
+        service_choice=prompt.select("Please choose your service?", %w(Service_view Drop_off_garments))   
         if service_choice=="Service_view"
              service_view()
-        elsif service_choice=="drop_off_garments"
+        elsif service_choice=="Drop_off_garments"
             run(customers)
         else
             break
             
         end
     end
+end
+
+
+
+def log_in(users,customers)
+    prompt = TTY::Prompt.new
+    name = prompt.select("Please choose the staff name?", %w(Heng  Hannah Upa Hevvon))
+    counter=0
+  while counter<3
+    if check_password(users,name)
+        puts "already login"
+        # puts "THANKS,#{user[:name]},You already logined." .colorize(:green)
+        # puts "----------------------------------".colorize(:green)
+        service(customers)
+        break
+    else
+        
+    end
+        counter+=1
+    end
+    puts "SORRY, you already enter three times, please reset your password.".colorize(:green)
+
+end
+
+
+
+
+
+def option(users,customers)
+    
+    prompt = TTY::Prompt.new
+    choose=prompt.select("Are you a staff or a customer?", %w(Staff Customer Exit))
+    if choose=="Staff"
+        log_in(users,customers)
+    elsif choose=="Customer"
+        service(customers)
     end 
 end
 option(users,customers)
